f70384f1a5665f9c7daa8a2de2d3300e23929b1f
Tyler Gaw <me@tylergaw.com>
2015-09-14 22:47:48 -0400

    Add initial twilio stuff

--- supporters_twilio/application.py
+++ supporters_twilio/application.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import twilio.twiml
 
 from requests.auth import HTTPBasicAuth
 from supporters_twilio.config import config_app
@@ -29,47 +30,52 @@ class Supporter:
         print req.text
         return req
 
-
-@app.route('/success/')
-def success():
-    return render_template(
-        'success.html',
-        email=request.args.get('email')
-    )
-
+#
+# @app.route('/success/')
+# def success():
+#     return render_template(
+#         'success.html',
+#         email=request.args.get('email')
+#     )
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
-    error = False
-    errorMsg = False
-    supporter = Supporter()
+def hello():
+    resp = twilio.twiml.Response()
+    resp.message('hi, robot')
+    return str(resp)
 
-    if request.method == 'POST':
-        payload = {
-            'givenName': request.form['givenName'],
-            'email': request.form['email'],
-            'source': request.form['source'],
-            'tags': {
-                'email_template': 'signup_supporters_example'
-            }
-        }
-
-        if not payload['givenName'] or not payload['email']:
-            error = True
-            errorMsg = 'We need both your name and email to sign you up.'
-        else:
-            req = supporter.create(payload)
-
-            if req.status_code == 201:
-                return redirect(url_for('success', email=payload['email']))
-            else:
-                error = True
-
-    return render_template(
-        'sign_up.html',
-        errorMsg=errorMsg,
-        error=error
-    )
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     error = False
+#     errorMsg = False
+#     supporter = Supporter()
+#
+#     if request.method == 'POST':
+#         payload = {
+#             'givenName': request.form['givenName'],
+#             'email': request.form['email'],
+#             'source': request.form['source'],
+#             'tags': {
+#                 'email_template': 'signup_supporters_example'
+#             }
+#         }
+#
+#         if not payload['givenName'] or not payload['email']:
+#             error = True
+#             errorMsg = 'We need both your name and email to sign you up.'
+#         else:
+#             req = supporter.create(payload)
+#
+#             if req.status_code == 201:
+#                 return redirect(url_for('success', email=payload['email']))
+#             else:
+#                 error = True
+#
+#     return render_template(
+#         'sign_up.html',
+#         errorMsg=errorMsg,
+#         error=error
+#     )
 
 if __name__ == '__main__':
     app.run()
